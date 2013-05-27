c40a3423b18d27b1e1b397881b35a5cd0336b8ff
StephanieFrias <stephanie.frias@gmail.com>
2013-05-26 20:48:41 -0400

    10 day challenge

--- chapter2_exercises.py
+++ chapter2_exercises.py
@@ -1,3 +1,40 @@
 # Exercises for chapter 2:
 
-# Name:
+# Name: Stephanie Frias
+
+#2.1 In base 8, aka octal, there are only 8 figures to work with: 0, 1, 2, 3, 4, 5, 6, 7. Therefore, 9 is an invalid figure, resulting in a syntax error.
+#When we enter 02132, the system believes we are in base eight. Which means (2*1)+(3*8)+(1*64)+(2*512)=1114
+
+#2.2
+
+print 5
+x=5
+print x+1
+
+#2.3
+
+width = 17
+height = 12.0
+delimiter = '.'
+print width/2
+print width/2.0
+print height/3
+print 1+2*5
+print delimiter * 5
+
+#2.4.1
+
+pi=3.1415926535897932
+print 4*(5**3)/3*pi
+
+#2.4.2
+
+print ((24.95*.6)*60)+((59*0.75)+3)
+
+#2.4.3
+
+print (8+(15/60.00))+(21+(36/60.00))+(8+(15/60.00))
+#the above calculates in minutes the sum of all three paced sections and tells us a total of 38.1 minutes elapsed
+print .1*60
+#the above tells us that 0.1 minutes equals 6 seconds
+#So, to arrive at the answer we add 38 minutes and 6 seconds to the time of 6:52:00am and get to 7:30:06am
--- chapter3_exercises.py
+++ chapter3_exercises.py
@@ -1,3 +1,53 @@
 # Exercises for chapter 3:
 
-# Name:
+# Name: Stephanie Frias
+
+#3.1 The script results in the following error: NameError: name 'repeat_lyrics' is not defined
+
+#3.2 Despite switching the order of the definitions, the script still produces the correct results and prints the lyrics twice. 
+
+def repeat_lyrics():
+	print_lyrics()
+	print_lyrics()
+
+def print_lyrics():
+	print "I'm a lumberjack, and I'm okay."
+	print "I sleep all night and I work all day."
+
+repeat_lyrics()
+
+#3.3
+
+def right_justify (s):
+	print ' '*(70-len(s)) + s
+
+right_justify ('allen')
+
+#3.4.1 
+
+def do_twice(f):
+	f()
+	f()
+
+def  print_spam():
+	print 'spam'
+
+do_twice(print_spam)
+
+#3.4.2 - #3.4.5
+
+def do_twice(f,value):
+	f(value)
+	f(value)
+
+def print_twice(string):
+	print string
+	print string
+
+do_twice(print_twice,'spam')
+
+def do_four(f,value2):
+	do_twice(f,value2)
+	do_twice(f, value2)
+	
+do_four(print_twice,'spam')
